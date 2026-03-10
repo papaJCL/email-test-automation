02179b09b8a1c40ef1b88a51b8de11159c843775
papaJCL <jeremylessermore@gmail.com>
2026-03-10 12:36:54 -0600

    Add the POM pseudo code pages

---
+++ pages/compose_page.py
@@ -0,0 +1,38 @@
+class ComposePage:
+    """
+    Page Object for the email compose screen.
+    Handles all interactions related to writing
+    and sending a new email.
+    """
+
+    def open_compose(self):
+        # Click the compose/new email button to open the compose window
+        pass
+
+    def fill_to_field(self, addresses):
+        # Locate the To input field and enter the email addresses
+        pass
+
+    def fill_cc_field(self, addresses):
+        # Locate the Cc input field and enter the email addresses
+        pass
+
+    def fill_bcc_field(self, addresses):
+        # Locate the Bcc input field and enter the email addresses
+        pass
+
+    def fill_subject(self, subject):
+        # Locate the subject input field and enter the subject text
+        pass
+
+    def fill_body(self, body):
+        # Locate the body text area and enter the email body
+        pass
+
+    def click_send(self):
+        # Click the send button to send the email
+        pass
+
+    def get_error_message(self):
+        # Locate and return any error message displayed after send attempt
+        pass
---
+++ pages/inbox_page.py
@@ -0,0 +1,39 @@
+class InboxPage:
+    """
+    Page Object for the inbox screen.
+    Handles all interactions related to viewing
+    and managing received emails.
+    """
+
+    def navigate_to_inbox(self):
+        # Click the inbox folder in the sidebar to navigate to inbox
+        pass
+
+    def get_email_list(self):
+        # Locate and return all email elements displayed in the inbox
+        pass
+
+    def get_email_count(self):
+        # Return the number of emails currently showing in the inbox
+        pass
+
+    def open_email(self, subject):
+        # Find an email by subject line and click to open it
+        pass
+
+    def get_email_sender(self):
+        # Return the sender address of the currently open email
+        pass
+
+    def get_email_recipients(self):
+        # Return the To, Cc, Bcc recipients of the currently open email
+        pass
+
+    def mark_as_spam(self, subject):
+        # Find an email by subject, select it, and click the mark as spam button
+        pass
+
+    def is_email_present(self, subject):
+        # Check if an email with the given subject exists in the inbox
+        # Returns True or False
+        pass
